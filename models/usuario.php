dc7f50e73e24a8d3dfd97e20c1a75c05322310c8
Gustavo Thiago Ribeiro Farina <161526981+GustavoHransa@users.noreply.github.com>
2024-10-30 11:56:51 -0300

    arquivo usuario ok

--- models/usuario.php
+++ models/usuario.php
@@ -1,4 +1,6 @@
 <?php
+
+echo "teste inicio ";
 class Usuario {
 
 protected $nome;
@@ -18,13 +20,34 @@ public function __contruct($nome,$email,$telefone,$cep){
 
 public function emprestar($livro){
 
-    
+    if(count($this->livros_emprestimos) < self :: max_emprestimo){
+        array_push($this->livros_emprestados,$livros);
+        return;
+    }
+}
+public function devolver($livro){
+    if(in_array($livro,$this->livros_emprestados)){
+        $livro->devolver_livros();
+        $position= array_searce($livro,$this->livros_emprestados);
+        unset($this-> qtd_livros [$position]);
 
 
+    }
+}
 
-
-
+public function criar($nome){
+    return "insert into usuario(nome,cep) values (".$this->nome.",".$this->cep.");";
+}
+public function ler(){
+    return "select * from usuario where nome= ".$this->nome.";";
+}
+public function atualizar(){
+    return "update usuario set nome=".$nome_atualizado. "where nome=".$this->nome.";";
+    }
+public function deletar(){
+    return "delete nome from usuario where=".$this->nome.";";
+}
 }
 
 
-}
+echo "Teste final ";
