6be0d22cc757f2bd4a28999faf80c5098926fcbd
KauaOSantos <kauaoliveira1506gaf@gmail.com>
2024-11-09 20:41:55 -0300

    Funções de Emprestimo 100%

--- models/emprestimo.php
+++ models/emprestimo.php
@@ -2,7 +2,7 @@
 
 class Emprestimo {
     private $conexao;
-    private $table_name = "emprestimos";
+    private $nome_tabela = "emprestimos";
 
     private $id;
     private $livro_id;
@@ -10,28 +10,57 @@ class Emprestimo {
     private $data_emprestimo;
     private $data_devolucao;
 
-    public function __construct($livro_id,$usuario_id) {
+    public function __construct($bd,$livro_id,$usuario_id) {
+        $this->conexao = $bd;
         $this->livro_id = $livro_id;
         $this->usuario_id = $usuario_id;
         $this->data_emprestmo = date("Y-m-d");
         $this->data_devoluçao = null;
     }
 
+
     public function realizarEmprestimo() {
-        
-    }
+        $query = "SELECT status FROM livros WHERE id = {$this->livro_id}";
+        // mysqli_query executa uma consulta no Banco de Dados
+        $resultado = mysqli_query($this->conexao, $query);
+        // já o mysqli_fetch_assoc Busca a próxima linha de um conjunto de resultados como um array associativo
+        $status = mysqli_fetch_assoc($resultado)['status'];
 
-
-    public function devolverLivro($emprestimo){
-        if($this->status == 'Disponível'){
-            echo 'Livro não está emprestado';
-            return;
+        if ($status != 'Disponível') {
+            echo "O livro não está disponível para empréstimo.";
+            return false;
         }
-        $this->usuarioEmprestado = null;
-        $this->status = 'Disponível';
+
+        $query = "UPDATE livros SET status = 'Emprestado' WHERE id = {$this->livro_id}";
+        mysqli_query($this->conexao, $query);
+
+        $query = "INSERT INTO emprestimos (livro_id, usuario_id, data_emprestimo) 
+                  VALUES ({$this->livro_id}, {$this->usuario_id}, '{$this->data_emprestimo}')";
+        return mysqli_query($this->conexao, $query);
     }
-}
+
+
+    public function devolverEmprestimo($emprestimo){
+        $data_devolucao = date("Y-m-d");
+        $query = "UPDATE emprestimos SET data_devolucao = '$data_devolucao' WHERE id = $emprestimo_id";
+        $resultado = mysqli_query($this->conexao, $query);
+
+        if ($resultado) {
+            $query = "UPDATE livros SET status = 'Disponível' WHERE id = {$this->livro_id}";
+            return mysqli_query($this->conexao, $query);
+        }
+        return false;
+    }
+
 
     public function verificarEmprestimos($usuario_id) {
+        $query = "SELECT * FROM emprestimos WHERE usuario_id = $usuario_id AND data_devolucao IS NULL";
+        $resultado = mysqli_query($this->conexao, $query);
+        $emprestimos = [];
 
+        while ($row = mysqli_fetch_assoc($resultado)) {
+            $emprestimos[] = $row;
+        }
+        return $emprestimos;
     }
+}
