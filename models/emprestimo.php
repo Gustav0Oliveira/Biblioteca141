e2e3a9c4d298a7bab0361abd070171f1bd36ddb8
KauaOSantos <kauaoliveira1506gaf@gmail.com>
2024-11-08 23:24:36 -0300

    Emprestimo 30%

--- models/emprestimo.php
+++ models/emprestimo.php
@@ -4,25 +4,34 @@ class Emprestimo {
     private $conexao;
     private $table_name = "emprestimos";
 
-    public $id;
-    public $usuario_id;
-    public $livro_id;
-    public $data_emprestimo;
-    public $data_devolucao;
+    private $id;
+    private $livro_id;
+    private $usuario_id;
+    private $data_emprestimo;
+    private $data_devolucao;
 
-    public function __construct($db) {
-        $this->conexao = $db;
+    public function __construct($livro_id,$usuario_id) {
+        $this->livro_id = $livro_id;
+        $this->usuario_id = $usuario_id;
+        $this->data_emprestmo = date("Y-m-d");
+        $this->data_devoluçao = null;
     }
 
     public function realizarEmprestimo() {
         
     }
 
-    public function devolverEmprestimo() {
 
+    public function devolverLivro($emprestimo){
+        if($this->status == 'Disponível'){
+            echo 'Livro não está emprestado';
+            return;
+        }
+        $this->usuarioEmprestado = null;
+        $this->status = 'Disponível';
     }
+}
 
     public function verificarEmprestimos($usuario_id) {
 
     }
-}
