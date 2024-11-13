ec3b225ceedff012b328e43742fdd4e9af6c6176
Gustav0Oliveira <gusoliveira2006@gmail.com>
2024-11-13 01:02:20 -0200

    KABUM

--- controllers/controllerUsuario.php
+++ controllers/controllerUsuario.php
@@ -3,64 +3,70 @@ require_once '.../interface/crud.php';
 require "../config/database.php";
 require "../models/usuario.php";
 
-class ControllerUsuario implements Crud{
+class ControllerUsuario implements Crud {
     protected $tabela = 'usuario';
 
-    public function create($nome,$cpf,$email,$cep,$Ncasa,$telfixo,$celular,$nascimento){
+    public function create($nome, $cpf, $email, $cep, $Ncasa, $telfixo, $celular, $nascimento, $senha) {
         $database = new Banco();
         $bd = $database->conectar();
     
+        
         $usuario = new Usuario($bd);
         $usuario->nome = $nome;
+        $usuario->cpf = $cpf;
         $usuario->email = $email;
-        $usuario->telefone = $telefone;
         $usuario->cep = $cep;
-        $this->Ncasa = $Ncasa; 
-        $this->telfixo = $telfixo;
-        $this->celular = $celular;
-        $this->nascimento = $nascimento; 
+        $usuario->Ncasa = $Ncasa; 
+        $usuario->telfixo = $telfixo;
+        $usuario->celular = $celular;
+        $usuario->nascimento = $nascimento;
+        $usuario->senha = $senha; 
     
-        if($usuario->create($nome,$cpf,$email,$cep,$Ncasa,$telfixo,$celular,$nascimento)){
-            header('Location: ./views/cadastar_livro.php');
+        if($usuario->create()) {
+            header('Location: index.php');
+            exit();
         } else {
             echo "Erro ao cadastrar usuario";    
         }
     }
 
-    public function read($arrayLer){
-        $database = new Banco();
-        $bd = $database->conectar();
-    
-        $usuario = new Usuario($bd);
-
-        if($usuario->read($arrayLer)){
-            header('Locatin: ./views/home.php');
-        } else {
-            echo 'Erro ao listar usuarios';
-        }
-    }
-
-    public function update($arrayAtualizar){
-        $database = new Banco();
-        $bd = $database->conectar();
-    
-        $usuario = new Usuario($bd);
-
-        if($usuario->update($arrayAtualizar)){
-            header('Location: atualizar.php');
-        } else {
-            echo 'Erro ao atualizar usuario';
-        }
-    }
-
-    public function delete($arrayDeletar){
+    public function read($id) {
         $database = new Banco();
         $bd = $database->conectar();
     
         $usuario = new Usuario($bd);
         
-        if($usuario->delete($arrayDeletar)){
+        if($usuario->read($id)) {
+            header('Location: listar.php');
+            exit();
+        } else {
+            echo 'Erro ao listar usuarios';
+        }
+    }
+
+    public function update($id, $nome_mudado) {
+        $database = new Banco();
+        $bd = $database->conectar();
+    
+        $usuario = new Usuario($bd);
+        
+        if($usuario->update($nome_mudado)) {
             header('Location: atualizar.php');
+            exit();
+        } else {
+            echo 'Erro ao atualizar usuario';
+        }
+    }
+
+    public function delete($id) {
+        $database = new Banco();
+        $bd = $database->conectar();
+    
+        $usuario = new Usuario($bd);
+        
+        if($usuario->delete($id)) {
+            header('Location: listar.php');
+            exit();
         } else {
             echo 'Erro ao excluir usuario';
         }
