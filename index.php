8b9b12033a794de72a1142e35dbca14182f9cbec
Gustav0Oliveira <gusoliveira2006@gmail.com>
2024-11-12 23:39:30 -0200

    Index atualizado para pagina inicial

--- index.php
+++ index.php
@@ -1,17 +1,3 @@
 <?php
-
-require_once './controllers/livroController.php';
-
-$acao = $_GET['acao'];
-
-switch($acao){
-    case 'cadastrar':
-        $livroController = new livroController();
-        $livroController->cadastrarLivro($_GET['titulo'], $_GET['autor'], $_GET['genero']); 
-        break;
-
-    default:
-        include 'views/cadastro_livro';
-
-
-}
+header('Location: views/login.php');
+?>
